df1f0588e6f274b59c63c8cf64886d43a2051acf
serena garuti <sere.garuti@gmail.com>
2019-11-19 17:56:00 +0000

    Job controller and routes created for index show and create views

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,30 @@
 class JobsController < ApplicationController
+  before_action :set_job, only: [:show]
+
+  def index
+    @jobs = Job.all
+  end
+
+  def show
+    @job
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    @job.save
+  end
+
+  private
+
+  def set_job
+    @job = Job.find(params[:id])
+  end
+
+  def job_params
+    params.require(:job).permit(:title, :school, :salary, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'jobs', to: 'jobs#index'
+  get 'jobs/new', to: 'jobs#new'
+  post 'jobs', to: 'jobs#create'
+  get 'jobs/:id', to: 'jobs#show'
 end
